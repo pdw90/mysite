f6d5c2b8aa43fb1d7c70f97a6ca2c3013ee6108c
pdw90 <daewoo.park@gmail.com>
2024-05-24 21:34:47 +0900

    포스트 수정 페이지에 태그 입력란 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -215,11 +215,15 @@ class TestView(TestCase):
         main_area = soup.find("div", id="main-area")
         self.assertIn("Create New Post", main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+
         self.client.post(
             "/blog/create_post/",
             {
                 "title": "Post Form 만들기",
                 "content": "Post Form 페이지를 만듭시다.",
+                "tags_str": "new tag; 한글 태그, python"
             },
         )
 
@@ -227,6 +231,11 @@ class TestView(TestCase):
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
         self.assertEqual(last_post.author.username, "obama")
+
+        self.assertEqual(last_post.tags.count(), 3)
+        self.assertTrue(Tag.objects.get(name='new tag'))
+        self.assertTrue(Tag.objects.get(name='한글 태그'))
+        self.assertEqual(Tag.objects.count(), 5)
 
     def test_update_post(self):
         update_post_url = f"/blog/update_post/{self.post_003.pk}/"
@@ -253,12 +262,17 @@ class TestView(TestCase):
         main_area = soup.find("div", id="main-area")
         self.assertIn("Edit Post", main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
                 "title": "세번째 포스트를 수정했습니다. ",
                 "content": "안녕 세계? 우리는 하나!",
                 "category": self.category_music.pk,
+                "tags_str": '파이썬 공부; 한글 태그, some tag'
             },
             follow=True,
         )
@@ -267,3 +281,4 @@ class TestView(TestCase):
         self.assertIn("세번째 포스트를 수정했습니다.", main_area.text)
         self.assertIn("안녕 세계? 우리는 하나!", main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
+        self.assertIn('파이썬 공부', main_area.text)
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post, Category, Tag
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
+
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
@@ -10,14 +12,15 @@ def tag_page(request, slug):
 
     return render(
         request,
-        'blog/post_list.html',
+        "blog/post_list.html",
         {
-            'post_list': post_list,
-            'tag': tag,
-            'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects.filter(category=None).count(),
-        }
+            "post_list": post_list,
+            "tag": tag,
+            "categories": Category.objects.all(),
+            "no_category_post_count": Post.objects.filter(category=None).count(),
+        },
     )
+
 
 class PostList(ListView):
     model = Post
@@ -39,9 +42,10 @@ class PostDetail(DetailView):
         context["no_category_post_count"] = Post.objects.filter(category=None).count()
         return context
 
+
 def category_page(request, slug):
-    if slug == 'no_category':
-        category = '미분류'
+    if slug == "no_category":
+        category = "미분류"
         post_list = Post.objects.filter(category=None)
     else:
         category = Category.objects.get(slug=slug)
@@ -49,18 +53,19 @@ def category_page(request, slug):
 
     return render(
         request,
-        'blog/post_list.html',
+        "blog/post_list.html",
         {
-            'post_list': post_list,
-            'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects.filter(category=None).count(),
-            'category': category,
-        }
+            "post_list": post_list,
+            "categories": Category.objects.all(),
+            "no_category_post_count": Post.objects.filter(category=None).count(),
+            "category": category,
+        },
     )
+
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ["title", "hook_text", "content", "head_image", "file_upload", "category"]
 
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
@@ -69,15 +74,41 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+
+            tags_str = self.request.POST.get("tags_str")
+            if tags_str:
+                tags_str = tags_str.strip()
+
+                tags_str = tags_str.replace(",", ";")
+                tags_list = tags_str.split(";")
+
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
         else:
-            return redirect('/blog/')
+            return redirect("/blog/")
+
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = [
+        "title",
+        "hook_text",
+        "content",
+        "head_image",
+        "file_upload",
+        "category",
+        "tags",
+    ]
 
-    template_name = 'blog/post_update_form.html'
+    template_name = "blog/post_update_form.html"
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
