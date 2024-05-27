fcb8282848e60c52a0afe645b3bfe930fe8bf80c
pdw90 <daewoo.park@gmail.com>
2024-05-27 23:25:30 +0900

    django-crispy-forms 적용하기

--- blog/models.py
+++ blog/models.py
@@ -55,4 +55,3 @@ class Post(models.Model):
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
 
-#test py
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+    'crispy_bootstrap4',
+
     'blog',
     'single_pages'
 ]
@@ -122,6 +126,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
